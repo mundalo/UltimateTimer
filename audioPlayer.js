19a10288d8ed44d8e46b1c0df05d441371179f1f
Mundal1994 <manga_flip@hotmail.com>
2020-12-16 16:07:41 +0100

    2nd try of fixing sound on phone

---
+++ audioPlayer.js
@@ -0,0 +1,43 @@
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+const sources = {};
+
+function audioPlayer(param) {
+
+  Object.keys(param).forEach(key => {
+    // sources[key] = audioCtx.createBufferSource();
+    var request = new XMLHttpRequest();
+
+    request.open('GET', param[key], true);
+
+    request.responseType = 'arraybuffer';
+
+
+    request.onload = function() {
+      var audioData = request.response;
+
+      audioCtx.decodeAudioData(audioData, function(buffer) {
+        sources[key] = buffer;
+          // sources[key].buffer = buffer;
+
+          // sources[key].connect(audioCtx.destination);
+          // sources[key].loop = false;
+        },
+
+        function(e) {
+          console.log("Error with decoding audio data" + e.err);
+        });
+
+    }
+
+    request.send();
+  });
+
+  return function playAudio(audioName) {
+    const source = audioCtx.createBufferSource();
+    source.buffer = sources[audioName];
+
+    source.connect(audioCtx.destination);
+    source.loop = false;
+    source.start(0)
+  }
+}
