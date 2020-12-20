2b5c78b8b887e90e7457b98df90072494596a53c
Mundal1994 <manga_flip@hotmail.com>
2020-12-20 18:05:17 +0100

    refactoring defaultprograms

---
+++ defaultPrograms.js
@@ -0,0 +1,119 @@
+const programs = [
+  {
+    //Select exercise
+    initialCountdown: 0,
+    exerciseInterval: 0,
+    restInterval: 0,
+    numberOfSets: 0,
+    recoveryInterval: 0,
+    numberOfCycles: 0,
+  },
+  {
+    //Sitting Pike Stretch
+    initialCountdown: 7,
+    exerciseInterval: 120,
+    restInterval: 30,
+    numberOfSets: 2,
+    recoveryInterval: 30,
+    numberOfCycles: 1,
+  },
+  {
+    //Sitting Leg Raise
+    initialCountdown: 7,
+    exerciseInterval: 8,
+    restInterval: 8,
+    numberOfSets: 8,
+    recoveryInterval: 30,
+    numberOfCycles: 3,
+  },
+  {
+    //Wall V-Sit
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 10,
+    numberOfSets: 9,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Weighted Jefferson Curl
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 10,
+    numberOfSets: 12,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Weight. St. Shoulder Extension
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 3,
+    numberOfSets: 8,
+    recoveryInterval: 10,
+    numberOfCycles: 4,
+  },
+  {
+    //Lying Weighted Stick Lift
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 5,
+    numberOfSets: 9,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Pre Side Split
+    initialCountdown: 7,
+    exerciseInterval: 120,
+    restInterval: 30,
+    numberOfSets: 2,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Horse Stance
+    initialCountdown: 7,
+    exerciseInterval: 30,
+    restInterval: 15,
+    numberOfSets: 5,
+    recoveryInterval: 30,
+    numberOfCycles: 3,
+  },
+  {
+    //Standing Side Leg Raise
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 5,
+    numberOfSets: 9,
+    recoveryInterval: 5,
+    numberOfCycles: 2,
+  },
+  {
+    //Pre Front Split
+    initialCountdown: 7,
+    exerciseInterval: 120,
+    restInterval: 10,
+    numberOfSets: 4,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Wall Quad Hip Flexor Stretch
+    initialCountdown: 7,
+    exerciseInterval: 120,
+    restInterval: 10,
+    numberOfSets: 4,
+    recoveryInterval: 0,
+    numberOfCycles: 1,
+  },
+  {
+    //Standing Leg Raise
+    initialCountdown: 7,
+    exerciseInterval: 10,
+    restInterval: 5,
+    numberOfSets: 5,
+    recoveryInterval: 5,
+    numberOfCycles: 6,
+  },
+];
